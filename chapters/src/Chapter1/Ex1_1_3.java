361809e8bdf2e543f20a1f94805fdb3148ab78ec
liujinxin <724959554@qq.com>
2019-11-14 12:26:56 +0800

    1

---
+++ chapters/src/Chapter1/Ex1_1_3.java
@@ -0,0 +1,16 @@
+package Chapter1;
+import edu.princeton.cs.algs4.*;
+
+public class Ex1_1_3 {
+    public static void main(String[] args) {
+        int a=StdIn.readInt();
+        int b=StdIn.readInt();
+        int c=StdIn.readInt();
+        if (a==b&&a==c)
+            System.out.printf("equal");
+        else
+            System.out.printf("not equal");
+
+
+    }
+}
---
+++ chapters/src/Chapter1/Test.java
@@ -0,0 +1,7 @@
+package Chapter1;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.printf("1+2+'3'");
+    }
+}
